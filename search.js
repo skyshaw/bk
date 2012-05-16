a92a4254bfbd1ad22873449f43de57f29e0c5975
skyshaw <pxtian2008@gmail.com>
2012-05-16 20:47:25 +0800

    use backreference in regex

--- search.js
+++ search.js
@@ -5,23 +5,41 @@ function search() {
            "https://www.google.com/bookmarks/find?hl=en&q=" + url + "&output=xml",
            true);
   xhr.onreadystatechange = function () {
-    var result, titles, linkers, li, link, i;
+    var result, titles = [], linkers = [], li, link, i;
+    var title_re = /<title>(.*?)<\/title>/g;
+    var url_re = /<url>(.*?)<\/url>/g;
+    var match;
     if (xhr.readyState === 4) {
-      // innerText does not let the attacker inject HTML elements.
-      titles = xhr.responseText.match(/<title>.*?<\/title>/g);
-      linkers = xhr.responseText.match(/<url>.*?<\/url>/g);
-      result = document.getElementById("result");
-      while (result.hasChildNodes()) {
-        result.removeChild(result.lastChild);
+      //titles = xhr.responseText.match(/<title>.*?<\/title>/g);
+      //linkers = xhr.responseText.match(/<url>.*?<\/url>/g);
+      //result = document.getElementById("result");
+      //while (result.hasChildNodes()) {
+      //  result.removeChild(result.lastChild);
+      //}
+      //for (i = 0; i < titles.length; ++i) {
+      //  li = document.createElement("li");
+      //  link = document.createElement("a");
+      //  link.href = linkers[i].replace("<url>", "").replace("</url>", "");
+      //  link.innerText = titles[i].replace("<title>", "").replace("</title>", "");
+      //  li.appendChild(link);
+      //  result.appendChild(li);
+      //}
+      while (match = title_re.exec(xhr.responseText)) {
+        titles.push(match[1]);
       }
+      while (match = url_re.exec(xhr.responseText)) {
+        linkers.push(match[1]);
+      }
+      result = document.getElementById("result");
       for (i = 0; i < titles.length; ++i) {
         li = document.createElement("li");
         link = document.createElement("a");
-        link.href = linkers[i].replace("<url>", "").replace("</url>", "");
-        link.innerText = titles[i].replace("<title>", "").replace("</title>", "");
+        link.href = linkers[i];
+        link.innerText = titles[i];
         li.appendChild(link);
         result.appendChild(li);
       }
+
     }
   };
   xhr.send();
